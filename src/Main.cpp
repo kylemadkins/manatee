3caab3b06ab52c8910bcf8a241368180065a477c
kylemadkins <kyle.m.adkins@gmail.com>
2023-05-13 01:24:15 -0400

    Fix includes

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,8 +3,8 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
-#include <glm.hpp>
-#include <imgui.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
 #include <sol/sol.hpp>
 
 int main() {
